f99dfd014461a71b896ea45552d5453de0450582
kjsa <kjsa@sjinc.co.kr>
2022-07-27 14:38:30 +0900

    Init

---
+++ src/main/java/com/kjsa/mybrowser/MyBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.kjsa.mybrowser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBrowserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBrowserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kjsa/mybrowser/MyBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjsa.mybrowser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBrowserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
